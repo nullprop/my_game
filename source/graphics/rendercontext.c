45eab2c408cc3299f750d4921cabc72fe691371a
nullprop <git@nullprop.sh>
2021-08-01 15:16:25 +0300

    Don't clear on immediate mode submit

--- source/graphics/rendercontext.c
+++ source/graphics/rendercontext.c
@@ -25,7 +25,14 @@ void render_ctx_update()
 {
     if (render_ctx_use_immediate_mode)
     {
-        gsi_render_pass_submit(&render_ctx_gsi, &render_ctx_cb, gs_color(0, 0, 0, 0));
+        //gsi_render_pass_submit(&render_ctx_gsi, &render_ctx_cb, gs_color(0, 0, 0, 0));
+        // custom impl without clear
+        gs_renderpass pass = gs_default_val();
+        gs_vec2 fb = gs_platform_framebuffer_sizev(gs_platform_main_window());
+        gs_graphics_begin_render_pass(&render_ctx_cb, pass);
+        gs_graphics_set_viewport(&render_ctx_cb, 0, 0, (int32_t)fb.x, (int32_t)fb.y);
+        gsi_draw(&render_ctx_gsi, &render_ctx_cb);
+        gs_graphics_end_render_pass(&render_ctx_cb);
     }
 
     // Submit command buffer (syncs to GPU, MUST be done on main thread where you have your GPU context created)
