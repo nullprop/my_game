2535b1d7f347f18cfa047564831da21a56043a7b
nullprop <git@nullprop.sh>
2022-08-13 16:19:18 +0300

    combine gcc scripts

---
+++ proc/linux/_gcc_base.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Clean
+rm -rf bin
+mkdir bin
+cd bin
+
+# Copy assets
+cp ../assets/ ./ -r
+cp ../src/shaders ./assets/ -r
+
+flags=(
+	-std=gnu99 -w -ldl -lGL -lX11 -pthread -lXi $1
+)
+
+inc=(
+	-I ../third_party/include/
+)
+
+libs=(
+	-lm
+)
+
+# Build game
+proj_name=game
+echo Building ${proj_name}...
+src=(
+	../src/main.c
+	../src/**/*.c
+)
+build_cmd="gcc ${inc[*]} ${src[*]} ${flags[*]} ${libs[*]} -o ${proj_name}"
+echo ${build_cmd}
+${build_cmd}
+
+if [ "$?" -ne "0" ]; then
+	exit 1
+fi
+
+if [ "$2" == "game" ]; then
+	exit 1
+fi
+
+# Build model viewer
+proj_name=modelviewer
+echo Building ${proj_name}...
+src=(
+	../src/model_viewer.c
+	../src/**/*.c
+)
+build_cmd="gcc ${inc[*]} ${src[*]} ${flags[*]} ${libs[*]} -o ${proj_name}"
+echo ${build_cmd}
+${build_cmd}
